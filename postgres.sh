dcece3270cfbdf72ee5ba4e6e85effff014aad53
dzianis zhurau <den_fobos@mail.ru>
2023-04-07 23:54:54 +0300

    changed postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-docker run -dp '5432:5432' --name humidity_db \
+mkdir -p home/"$USER"/db/climate
+docker run --rm -dp '5432:5432' --name humidity_db \
 -e POSTGRES_PASSWORD=N@ut1lus -e POSTGRES_USER=fobos -e POSTGRES_DB=humidity \
 --network climate --network-alias humidity \
+-v /home/fobos/db/climate:/var/lib/postgresql/data \
 postgres:11-alpine
-#-v /home/fobos/db/climate:/var/lib/postgresql/data \
