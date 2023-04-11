68d1036562ca8aa6f10741963230ba9319ee0012
dzhurau <dzhurau@student.42wolfsburg.de>
2023-04-11 22:42:07 +0300

    fix: refactor some files, changed Dockerfile

--- src/_index.js
+++ src/_index.js
@@ -17,39 +17,88 @@ app.use(cors())
 
 app.get('/climate', (req, res) => {
 
-    console.log(req.socket.remoteAddress);
-    const roomData = getRoomTemp(sensorAddress); // add condition where sourse is unavailable
-    roomData.then(data => {
-        res.send(data);
-    })
+	console.log(req.socket.remoteAddress);
+	const roomData = getRoomTemp(sensorAddress); // add condition where sourse is unavailable
+	roomData.then(data => {
+		res.send(data);
+	})
 });
 
 app.get('/esp', (req, res) => {
-    const esp = require('../public/esp.html');
-    res.send(esp);
+	const esp = require('../public/esp.html');
+	res.send(esp);
 });
 
 app.get('/update-sensor', (req, res) => {
-    console.log(req.query);
-    res.send('ok').status(200);
+	console.log(req.query);
+	res.send('ok').status(200);
 });
 
 app.post('/post-sensor-data', (req, res) => {
-    // console.log(req.headers);
-    if (typeof req.body === 'object') {
-        console.log(req.body);
-        res.sendStatus(200);
-    } else {
-        try {
-            body = JSON.parse(req.body);
-            console.log(body);
-            res.sendStatus(200);
-        } catch {
-            res.send('JSON only accepted').status(406);
-        }
-    }
+	// console.log(req.headers);
+	if (typeof req.body === 'object') {
+		console.log(req.body);
+		res.sendStatus(200);
+	} else {
+		try {
+			body = JSON.parse(req.body);
+			console.log(body);
+			res.sendStatus(200);
+		} catch {
+			res.send('JSON only accepted').status(406);
+		}
+	}
 })
 
 app.listen(port, () => {
-    console.log('Server has started on port ' + port);
+	console.log('Server has started on port ' + port);
 });
+
+
+// const sequelize = require('sequelize');
+// var { Sequelize } = require('sequelize');
+// const sequelize = require('./libraries/Database');
+
+// var me = User.create({
+//     name: 'Denis Zhurov',
+//     email: 'den_fobos@mail.ru',
+//     password: '123456',
+
+// }).then(() => {
+//     console.log('User created')
+// }).catch((e) => {
+//     console.log('Failed to add user to db', e);
+// })
+
+// var users = User.findAll();
+// users.then(data => {
+//     data.every(user => console.log(user.getDataValue('email')));
+// });
+
+// server.on('request', (req, res) => {
+//     const { url, method, headers } = req;
+//     // console.log(url);
+//     // console.log(method);
+//     // console.log(headers);
+//     if (headers['content-type'] == 'application/json' && method === 'POST') {
+//         let body = [];
+//         req.on('data', (chunk) => {
+//             body.push(chunk)
+//         }).on('end', () => {
+//             body = Buffer.concat(body).toString();
+//             console.log(JSON.parse(body));
+//         });
+//     }
+//     res.write("hello world");
+//     res.statusCode = 200;
+//     res.end();
+//     req.on('error', () => {
+//         console.log('Error during http request');
+//         res.setHeader(503);
+//         res.end('error on server');
+//     })
+// });
+// server.on('error', (e) => {
+//     console.log('server error was heppend');
+//     console.log(e);
+// });
